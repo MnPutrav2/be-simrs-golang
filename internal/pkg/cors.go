4433364d97d0000b26b7f40be8cef4f72b21ebef
MnPutrav2 <mochnurdinv2@gmail.com>
2025-06-06 20:15:17 +0700

    update method

--- internal/pkg/cors.go
+++ internal/pkg/cors.go
@@ -14,7 +14,7 @@ func Cors(w http.ResponseWriter, r *http.Request) bool {
 	}
 
 	w.Header().Set("Access-Control-Allow-Origin", os.Getenv("ALLOW_ORIGIN"))
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE, PATCH")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE, PUT")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 	if r.Method == http.MethodOptions {
