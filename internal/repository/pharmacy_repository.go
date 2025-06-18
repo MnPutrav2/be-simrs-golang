83f578d06961c74e6166a192170fee0e172f4b76
MnPutrav2 <mochnurdinv2@gmail.com>
2025-06-18 20:47:51 +0700

    update repository

--- internal/repository/pharmacy_repository.go
+++ internal/repository/pharmacy_repository.go
@@ -1,7 +1,24 @@
 package repository
 
-import "github.com/MnPutrav2/be-simrs-golang/internal/models"
+import (
+	"database/sql"
+
+	"github.com/MnPutrav2/be-simrs-golang/internal/models"
+)
 
 type PharmacyRepository interface {
 	CreateDrugData(drug models.DrugData) error
+}
+
+type pharmacyRepository struct {
+	sql *sql.DB
+}
+
+func NewPharmacyRepository(sql *sql.DB) PharmacyRepository {
+	return &pharmacyRepository{sql}
+}
+
+func (q *pharmacyRepository) CreateDrugData(drug models.DrugData) error {
+	_, err := q.sql.Exec("INSERT INTO drug_datas(id, name, distributor, capacity, unit, price, category, expired_date) VALUES(?, ?, ?, ?, ?, ?, ?, ?)", drug.ID, drug.Name, drug.Distributor, drug.Capacity, drug.Unit, drug.Price, drug.Category, drug.ExpiredDate)
+	return err
 }
--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -1,9 +1,80 @@
 package repository
 
-import "github.com/MnPutrav2/be-simrs-golang/internal/models"
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/MnPutrav2/be-simrs-golang/internal/helper"
+	"github.com/MnPutrav2/be-simrs-golang/internal/models"
+)
 
 type UserRepository interface {
 	GetUserPagesData(token string, path string) ([]models.UserPages, error)
 	GetUserStatus(token string, path string) (models.EmployeeData, error)
 	UserLogout(token string) error
+}
+
+type userRepository struct {
+	w   http.ResponseWriter
+	r   *http.Request
+	sql *sql.DB
+}
+
+func NewUserRepository(w http.ResponseWriter, r *http.Request, sql *sql.DB) UserRepository {
+	return &userRepository{w, r, sql}
+}
+
+func (q *userRepository) GetUserPagesData(token string, path string) ([]models.UserPages, error) {
+	var id int
+
+	err := q.sql.QueryRow("SELECT session_token.users_id FROM session_token WHERE session_token.token = ?", token).Scan(&id)
+	if err != nil {
+		helper.ResponseError(q.w, 0, "unauthorization", "unauthorization : 400", 401, path)
+		return nil, err
+	}
+
+	result, err := q.sql.Query("SELECT user_pages.name, user_pages.path FROM user_pages WHERE user_pages.users_id = ?", id)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var pageList []models.UserPages
+
+	for result.Next() {
+		var p models.UserPages
+
+		err := result.Scan(&p.Name, &p.Path)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		pageList = append(pageList, p)
+	}
+
+	return pageList, err
+}
+
+func (q *userRepository) GetUserStatus(token string, path string) (models.EmployeeData, error) {
+	var id int
+
+	err := q.sql.QueryRow("SELECT session_token.users_id FROM session_token WHERE session_token.token = ?", token).Scan(&id)
+	if err != nil {
+		helper.ResponseError(q.w, 0, "unauthorization", "unauthorization : 400", 401, path)
+		return models.EmployeeData{}, err
+	}
+
+	var user models.EmployeeData
+
+	err = q.sql.QueryRow("SELECT employee.id, employee.name, employee.gender, employee.birth_place, employee.birth_date, employee.address, employee.village, employee.district, employee.regencie, employee.province, employee.nik, employee.bpjs, employee.npwp, employee.phone_number, employee.email FROM employee INNER JOIN users ON employee.id = users.employee_id WHERE users.id = ?", id).Scan(&user.Employee_ID, &user.Name, &user.Gender, &user.BirthPlace, &user.BirthDate, &user.Address, &user.Village, &user.District, &user.Regencie, &user.Province, &user.NIK, &user.BPJS, &user.NPWP, &user.PhoneNumber, &user.Email)
+	if err != nil {
+		helper.ResponseError(q.w, 0, "employee data not found", "employee data not found : 404", 404, path)
+		return models.EmployeeData{}, err
+	}
+
+	return user, err
+}
+
+func (q *userRepository) UserLogout(token string) error {
+	_, err := q.sql.Exec("DELETE FROM session_token WHERE session_token.token = ?", token)
+	return err
 }
