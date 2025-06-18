28383ffb3e41609fd7711397fc3e1e7ad2783a5c
MnPutrav2 <mochnurdinv2@gmail.com>
2025-06-18 21:27:44 +0700

    update pharmacy model

--- internal/models/pharmacy_model.go
+++ internal/models/pharmacy_model.go
@@ -1,14 +1,12 @@
 package models
 
-import "time"
-
 type DrugData struct {
-	ID          int       `json:"id"`
-	Name        string    `json:"name"`
-	Distributor string    `json:"distributor"`
-	Capacity    string    `json:"capacity"`
-	Unit        string    `json:"unit"`
-	Category    string    `json:"category"`
-	Price       int       `json:"price"`
-	ExpiredDate time.Time `json:"expired_date"`
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Distributor string `json:"distributor"`
+	Capacity    string `json:"capacity"`
+	Unit        string `json:"unit"`
+	Category    string `json:"category"`
+	Price       int    `json:"price"`
+	ExpiredDate string `json:"expired_date"`
 }
